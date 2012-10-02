537dedb82bf27abdb6999cb10aa2fe1d28862a22
Raphael Bost <raphoufoune@gmail.com>
2012-10-02 17:17:12 -0400

    Initial Commit

---
+++ ERAppKit/ERAppKit.h
@@ -0,0 +1,13 @@
+//
+//  ERAppKit.h
+//  ERAppKit
+//
+//  Created by Raphael Bost on 02/10/12.
+//  Copyright (c) 2012 Evan Altman, Raphael Bost. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ERAppKit : NSObject
+
+@end
---
+++ ERAppKit/ERAppKit.m
@@ -0,0 +1,13 @@
+//
+//  ERAppKit.m
+//  ERAppKit
+//
+//  Created by Raphael Bost on 02/10/12.
+//  Copyright (c) 2012 Evan Altman, Raphael Bost. All rights reserved.
+//
+
+#import "ERAppKit.h"
+
+@implementation ERAppKit
+
+@end
