adeda31c3b97eca97c9aba1e69f42bcf8b862473
Evan Altman <evanaltman1@yahoo.com>
2012-12-06 11:58:54 -0500

    merged the two "downTabs" holders into one

--- ERAppKitDemo/AppDelegate.m
+++ ERAppKitDemo/AppDelegate.m
@@ -34,21 +34,20 @@
     ERPaletteTabView *upTabs = [[self paletteHolder] addTabViewWithPosition:ERPalettePanelPositionUp];
     ERPaletteTabView *leftTabs = [[self paletteHolder] addTabViewWithPosition:ERPalettePanelPositionLeft];
     ERPaletteTabView *rightTabs = [[self paletteHolder] addTabViewWithPosition:ERPalettePanelPositionRight];
-    ERPaletteTabView *downTabs1 = [[self paletteHolder] addTabViewWithSize:200 location:[ERPaletteTabView barThickness] position:ERPalettePanelPositionDown];
-    ERPaletteTabView *downTabs2 = [[self paletteHolder] addTabViewWithSize:200 location:200+2*[ERPaletteTabView barThickness] position:ERPalettePanelPositionDown];
+    ERPaletteTabView *downTabs = [[self paletteHolder] addTabViewWithPosition:ERPalettePanelPositionDown];
 
     [upTabs setAutoresizingMask:(NSViewWidthSizable|NSViewMinYMargin)];
     [leftTabs setAutoresizingMask:(NSViewHeightSizable|NSViewMaxXMargin)];
     [rightTabs setAutoresizingMask:(NSViewHeightSizable|NSViewMinXMargin)];
-    [downTabs1 setAutoresizingMask:(NSViewWidthSizable|NSViewMaxYMargin|NSViewMaxXMargin)];
-    [downTabs2 setAutoresizingMask:(NSViewWidthSizable|NSViewMaxYMargin|NSViewMinXMargin)];
+    [downTabs setAutoresizingMask:(NSViewWidthSizable|NSViewMaxYMargin|NSViewMaxXMargin)];
+   
     
     [upTabs addPaletteWithContentView:[self paletteContent1] icon:[NSImage imageNamed:@"tool-arrow"] title:@"Content 1"];
     [leftTabs addPaletteWithContentView:[self paletteContent2] icon:[NSImage imageNamed:@"tool-bezier"] title:@"Content 2"];
     [rightTabs addPaletteWithContentView:[self paletteContent3] icon:[NSImage imageNamed:@"tool-text"] title:@"Content 3"];
-    [downTabs1 addPaletteWithContentView:[self paletteContent4] icon:[NSImage imageNamed:@"tool-oval"] title:@"Content 4"];
+    [downTabs addPaletteWithContentView:[self paletteContent4] icon:[NSImage imageNamed:@"tool-oval"] title:@"Content 4"];
     [upTabs addPaletteWithContentView:[self paletteContent5] icon:[NSImage imageNamed:@"tool-rectangle"] title:@"Content 5"];
-    [downTabs2 addPaletteWithContentView:[self paletteContent6] icon:[NSImage imageNamed:@"tool-zoom"] title:@"Content 6"];    
+    [downTabs addPaletteWithContentView:[self paletteContent6] icon:[NSImage imageNamed:@"tool-zoom"] title:@"Content 6"];
 }
 
 - (void)showDummyMenu:(NSEvent *)event
